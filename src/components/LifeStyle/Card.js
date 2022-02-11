245674f9d47880b9e95ba6462728979455f2b67d
risingCore <vcjsm2283@gmail.com>
2022-02-11 11:39:45 +0900

    [style] common : 공통화 컴포넌트 작업 및 페이지 scrollTo 설정
    NavLink 를 클릭하였을때, active 값과, useEffect를 이용한 페이지 이동시 scrollTo 으로 최상단에 머무르게 하도록 구현

--- src/components/LifeStyle/Card.js
+++ src/components/LifeStyle/Card.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { Fragment } from "react";
+import styled from "styled-components";
+import { CardCaption, CardImage, WrapperDiv } from "../common/Common";
 
 const StyledLi = styled.li`
   width: calc(33.33% - 40px);
@@ -9,100 +10,122 @@ const StyledLi = styled.li`
   &:nth-child(9) {
     width: calc(66.67% - 40px);
   }
-  img {
-    width: 100%;
-    height: 470px;
-    background-color: #ddd;
-  }
-  dl {
-    margin-top: 20px;
-    color: #191919;
-    dt {
-      font-size: 16px;
-      font-weight: 700;
-      text-transform: uppercase;
-    }
-    dd {
-      margin-top: 8px;
-      font-size: 24px;
-    }
-  }
 `;
 
 function Card() {
   return (
-    <>
+    <Fragment>
       <StyledLi>
-        <img src='' alt='' />
-        <dl>
-          <dt>trend</dt>
-          <dd>이커머스가 쏘아 올린 구독</dd>
-        </dl>
+        <WrapperDiv>
+          <CardImage height={"470px"}>
+            <img src="" alt="" />
+          </CardImage>
+          <CardCaption>
+            <dt>trend</dt>
+            <dd>이커머스가 쏘아 올린 구독</dd>
+          </CardCaption>
+        </WrapperDiv>
       </StyledLi>
       <StyledLi>
-        <img src='' alt='' />
-        <dl>
-          <dt>trend</dt>
-          <dd>이커머스가 쏘아 올린 구독</dd>
-        </dl>
+        <WrapperDiv>
+          <CardImage height={"470px"}>
+            <img src="" alt="" />
+          </CardImage>
+          <CardCaption>
+            <dt>trend</dt>
+            <dd>이커머스가 쏘아 올린 구독</dd>
+          </CardCaption>
+        </WrapperDiv>
       </StyledLi>
       <StyledLi>
-        <img src='' alt='' />
-        <dl>
-          <dt>trend</dt>
-          <dd>이커머스가 쏘아 올린 구독</dd>
-        </dl>
+        <WrapperDiv>
+          <CardImage height={"470px"}>
+            <img src="" alt="" />
+          </CardImage>
+          <CardCaption>
+            <dt>trend</dt>
+            <dd>이커머스가 쏘아 올린 구독</dd>
+          </CardCaption>
+        </WrapperDiv>
       </StyledLi>
       <StyledLi>
-        <img src='' alt='' />
-        <dl>
-          <dt>trend</dt>
-          <dd>이커머스가 쏘아 올린 구독</dd>
-        </dl>
+        <WrapperDiv>
+          <CardImage height={"470px"}>
+            <img src="" alt="" />
+          </CardImage>
+          <CardCaption>
+            <dt>trend</dt>
+            <dd>이커머스가 쏘아 올린 구독</dd>
+          </CardCaption>
+        </WrapperDiv>
       </StyledLi>
       <StyledLi>
-        <img src='' alt='' />
-        <dl>
-          <dt>trend</dt>
-          <dd>이커머스가 쏘아 올린 구독</dd>
-        </dl>
+        <WrapperDiv>
+          <CardImage height={"470px"}>
+            <img src="" alt="" />
+          </CardImage>
+          <CardCaption>
+            <dt>trend</dt>
+            <dd>이커머스가 쏘아 올린 구독</dd>
+          </CardCaption>
+        </WrapperDiv>
       </StyledLi>
       <StyledLi>
-        <img src='' alt='' />
-        <dl>
-          <dt>trend</dt>
-          <dd>이커머스가 쏘아 올린 구독</dd>
-        </dl>
+        <WrapperDiv>
+          <CardImage height={"470px"}>
+            <img src="" alt="" />
+          </CardImage>
+          <CardCaption>
+            <dt>trend</dt>
+            <dd>이커머스가 쏘아 올린 구독</dd>
+          </CardCaption>
+        </WrapperDiv>
       </StyledLi>
       <StyledLi>
-        <img src='' alt='' />
-        <dl>
-          <dt>trend</dt>
-          <dd>이커머스가 쏘아 올린 구독</dd>
-        </dl>
+        <WrapperDiv>
+          <CardImage height={"470px"}>
+            <img src="" alt="" />
+          </CardImage>
+          <CardCaption>
+            <dt>trend</dt>
+            <dd>이커머스가 쏘아 올린 구독</dd>
+          </CardCaption>
+        </WrapperDiv>
       </StyledLi>
       <StyledLi>
-        <img src='' alt='' />
-        <dl>
-          <dt>trend</dt>
-          <dd>이커머스가 쏘아 올린 구독</dd>
-        </dl>
+        <WrapperDiv>
+          <CardImage height={"470px"}>
+            <img src="" alt="" />
+          </CardImage>
+          <CardCaption>
+            <dt>trend</dt>
+            <dd>이커머스가 쏘아 올린 구독</dd>
+          </CardCaption>
+        </WrapperDiv>
       </StyledLi>
       <StyledLi>
-        <img src='' alt='' />
-        <dl>
-          <dt>trend</dt>
-          <dd>이커머스가 쏘아 올린 구독</dd>
-        </dl>
+        <WrapperDiv>
+          <CardImage height={"470px"}>
+            <img src="" alt="" />
+          </CardImage>
+          <CardCaption>
+            <dt>trend</dt>
+            <dd>이커머스가 쏘아 올린 구독</dd>
+          </CardCaption>
+        </WrapperDiv>
       </StyledLi>
       <StyledLi>
-        <img src='' alt='' />
-        <dl>
-          <dt>trend</dt>
-          <dd>이커머스가 쏘아 올린 구독</dd>
-        </dl>
+        <WrapperDiv>
+          <CardImage height={"470px"}>
+            <img src="" alt="" />
+          </CardImage>
+          <CardCaption>
+            <dt>trend</dt>
+            <dd>이커머스가 쏘아 올린 구독</dd>
+          </CardCaption>
+        </WrapperDiv>
       </StyledLi>
-    </>
+    </Fragment>
   );
 }
 
