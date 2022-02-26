32b9ef772ba701a4b1405b5edbcee35934d36f8a
SA K <sakang07@gmail.com>
2022-02-26 16:35:51 +0900

    [feat] Router : Set sub router of LifeStyle.js

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -7,13 +7,16 @@ import ShowCase from '../pages/ShowCase';
 import Login from '../pages/Login';
 import Join from '../pages/Join';
 import MyPage from '../pages/MyPage';
+import LifeStyleDetail from '../components/LifeStyle/LifeStyleDetail';
 
 const Router = () => {
   return (
     <Routes>
       <Route path='/' element={<Main />} />
       <Route path='/showcase' element={<ShowCase />} />
-      <Route path='/lifestyle' element={<LifeStyle />} />
+      <Route path='/lifestyle' element={<LifeStyle />}>
+        <Route path=':id' element={<LifeStyleDetail />} />
+      </Route>
       <Route path='/single-edit' element={<SingleEdit />} />
       <Route path='/login' element={<Login />} />
       <Route path='/join' element={<Join />} />
---
+++ src/components/LifeStyle/LifeStyleDetail.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LifeStyleDetail() {
+  return <div>LifeStyleDetail</div>;
+}
+
+export default LifeStyleDetail;
