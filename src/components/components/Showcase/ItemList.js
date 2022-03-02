7dc246ca4da40cbe5218b89738c921047d7e8fab
op <wenigwaves@gmail.com>
2022-03-02 18:00:06 +0900

    [feat]ItemList.js: create file

---
+++ src/components/components/Showcase/ItemList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const ItemList = styled.div`
+  figure {
+    width: 290px;
+    height: 290px;
+    margin-bottom: 20px;
+    background-color: #fad; }
+  figcaption {
+    font-size: 24px;
+    color: #fff; }
+`;
+
+
