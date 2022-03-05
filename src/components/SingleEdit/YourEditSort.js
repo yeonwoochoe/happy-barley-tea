e0248b00ba36befc73d071d24b91279c8a4f54dc
yeonwoo <chiyy6162@gmail.com>
2022-03-05 18:14:40 +0900

    [feat] singleEdit : Your Edit 소분류 카테고리 적용

---
+++ src/components/SingleEdit/YourEditSort.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { SortAllList } from "../common/Common";
+
+const YourEditSort = (props) => {
+  const listArr = ["ALL", "#ITEM", "#SWEATLIFE", "#OOTD", "#YUMMY", "#ROUTINE"];
+  const [sortData, setSortData] = useState(listArr[0]);
+  console.log(props);
+
+  const sortHandler = (el) => {
+    setSortData(el);
+    if (el === listArr[0]) {
+      props.sort[0]();
+    } else {
+      props.sort[1]();
+    }
+  };
+
+  return (
+    <SortAllList>
+      <ul>
+        {props.category === "Expert's Edit" ? (
+          <ul>
+            {listArr.map((el, idx) => (
+              <li key={idx}>
+                <button
+                  type="button"
+                  onClick={() => sortHandler(el)}
+                  style={
+                    sortData === el ? { color: "#000" } : { color: "#9f9f9f" }
+                  }
+                >
+                  {el}
+                </button>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          " "
+        )}
+      </ul>
+    </SortAllList>
+  );
+};
+
+export default YourEditSort;
