eadb318477aaf3873d4139e4082a43585b5aa8ee
risingCore <vcjsm2283@gmail.com>
2022-03-24 10:28:50 +0900

    [feat] : create .env file .gitignore firebase-config
    .env 파일에 API_KEY 입력 및 process.env 실행

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,18 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
-import 'firebase/database';
-import 'firebase/auth';
-import 'firebase/storage';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import "firebase/database";
+import "firebase/auth";
+import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDBSosJ_FQCuBg78Sz0ecOYnGNtOM_4v8s',
-  authDomain: 'the-single-plus.firebaseapp.com',
-  databaseURL: 'https://the-single-plus-default-rtdb.firebaseio.com',
-  projectId: 'the-single-plus',
-  storageBucket: 'the-single-plus.appspot.com',
-  messagingSenderId: '991118539545',
-  appId: '1:991118539545:web:454982552f3923eaec6388',
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
